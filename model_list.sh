8ecc7d636dd81d89fe50c0b7c9779e2bdcece95f
Amit Maity <maity.2005@gmail.com>
2026-05-07 19:24:32 -0700

    Update model_list.sh

--- model_list.sh
+++ model_list.sh
@@ -30,7 +30,9 @@ wget https://huggingface.co/Lightricks/LTX-2.3-22b-IC-LoRA-Motion-Track-Control/
 
 wget https://huggingface.co/LiconStudio/Ltx2.3-VBVR-lora-I2V/resolve/main/Ltx2.3-Licon-VBVR-I2V-390K-R32.safetensors
 
-wget https://huggingface.co/Comfy-Org/ltx-2.3/resolve/main/split_files/loras/ltx-2.3-id-lora-talkvid-3k.safetensors
+wget https://huggingface.co/Lightricks/LTX-2-19b-LoRA-Camera-Control-Static/blob/main/ltx-2-19b-lora-camera-control-static.safetensors
 
-wget https://huggingface.co/Comfy-Org/ltx-2.3/resolve/main/split_files/loras/ltx-2.3-id-lora-celebvhq-3k.safetensors
+#wget https://huggingface.co/Comfy-Org/ltx-2.3/resolve/main/split_files/loras/ltx-2.3-id-lora-talkvid-3k.safetensors
+
+#wget https://huggingface.co/Comfy-Org/ltx-2.3/resolve/main/split_files/loras/ltx-2.3-id-lora-celebvhq-3k.safetensors
 
